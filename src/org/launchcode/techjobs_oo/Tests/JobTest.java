e3d7a6daf2e929b88426588946a630e5ae4f522f
Edward Rodriquez <Ed.R.Rodriquez@gmail.com>
2020-09-21 20:41:09 -0600

    Added Tests.java, completed id check and both constructor tests

---
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,50 @@
+package org.launchcode.techjobs_oo.Tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.launchcode.techjobs_oo.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class JobTest {
+
+    Job test_job1;
+    Job test_job2;
+    Job test_job3;
+    Job test_job4;
+    //set job objects for future testing
+
+    @Before
+
+    public void testJobObjects() {
+        test_job1 = new Job();
+        test_job2 = new Job();
+        test_job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        test_job4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+    }
+
+
+    @Test
+//theoretically each Job object id should not be the same, each job id different by 1
+    public void testSettingJobId() {
+        assertTrue(!test_job1.equals(test_job2));
+        assertTrue(test_job1.getId() + 1 == test_job2.getId());
+    }
+
+    @Test
+    // each Job object should contain 6 fields & proper data types
+    //int id, String name, Employer employer, Location location, PostiionType postiontype, CoreCompetency corecompetency
+
+    public void testJobConstructorSetsAllFields() {
+        assertTrue(test_job3 instanceof Job);
+        assertEquals(3.0, test_job3.getId(), .001);
+        assertTrue(test_job3.getName() == "Product tester");
+        assertTrue(test_job3.getEmployer().getValue() == "ACME");
+        assertTrue(test_job3.getLocation().getValue() == "Desert");
+        assertTrue(test_job3.getPositionType().getValue() == "Quality control");
+        assertTrue(test_job3.getCoreCompetency().getValue() == "Persistence");
+
+    }
+
+}
